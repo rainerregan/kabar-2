d364fac6a1270763fd5fe9cf1fac169cd6c5e521
Rainer Regan <rainercoc@gmail.com>
2022-05-10 10:39:44 +0700

    Hiding API KEYS

--- app/src/main/java/com/merahputih/kabar/MainApp.java
+++ app/src/main/java/com/merahputih/kabar/MainApp.java
@@ -3,6 +3,7 @@ package com.merahputih.kabar;
 import android.app.Activity;
 import android.app.Application;
 import android.graphics.drawable.Drawable;
+import android.util.Base64;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -11,9 +12,13 @@ import com.merahputih.kabar.custom_activities.CustomMainAppBarActivity;
 import com.merahputih.kabar.custom_activities.CustomPostRegistrationActivity;
 import com.merahputih.kabar.interface_adapter.MyAppInterfaceAdapter;
 import com.merahputih.kabar.settings.Constants;
+import com.mikepenz.iconics.IconicsDrawable;
 
 import sdk.chat.app.firebase.ChatSDKFirebase;
+import sdk.chat.core.interfaces.ChatOption;
 import sdk.chat.core.session.ChatSDK;
+import sdk.chat.message.location.LocationChatOption;
+import sdk.chat.ui.chat.options.ChatOptionBuilder;
 
 public class MainApp extends Application {
     @Override
@@ -22,19 +27,34 @@ public class MainApp extends Application {
 
         // Chat SDK
         try {
-            ChatSDKFirebase.quickStart(this, "pre_1", Constants.MapsSettings.STATIC_MAP_API_KEY, true);
+
+            byte[] data = Base64.decode(Constants.MapsSettings.STATIC_MAP_KEY, Base64.DEFAULT);
+            String KEY = new String(data, "UTF-8");
+
+            ChatSDKFirebase.quickStart(this, "pre_1", KEY, true);
             ChatSDK.config().setLogoDrawableResourceID(R.drawable.kabar_icon_2);
 
             // Set Interface Adapter untuk setting custom layout.
             // Custom layout dapat berupa custom class yang inherit class aslinya.
             // Override pada adapter akan mengganti layout default.
             ChatSDK.shared().setInterfaceAdapter(new MyAppInterfaceAdapter(getApplicationContext()));
-
             ChatSDK.ui().setPostRegistrationActivity(CustomPostRegistrationActivity.class);
 
-            ChatSDK.ui().removeTab(1);
+            setChatOptions();
+            setTabsProperties();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private void setChatOptions() {
+        ChatOption option = new LocationChatOption("📍 Location");
+        ChatSDK.ui().addChatOption(option);
+    }
+
+    private void setTabsProperties() {
+        ChatSDK.ui().removeTab(1);
+        ChatSDK.ui().tabs().get(0).title = "Percakapan";
+        ChatSDK.ui().tabs().get(1).title = "Kontak";
     }
 }
--- app/src/main/java/com/merahputih/kabar/settings/Constants.java
+++ app/src/main/java/com/merahputih/kabar/settings/Constants.java
@@ -3,7 +3,7 @@ package com.merahputih.kabar.settings;
 public class Constants {
 
     public static class MapsSettings{
-        public static final String STATIC_MAP_API_KEY = "AIzaSyBY4Sms4p6neeOi5lptv4G-O65wPz15w5w";
+        public static final String STATIC_MAP_KEY = "QUl6YVN5Qlk0U21zNHA2bmVlT2k1bHB0djRHLU82NXdQejE1dzV3";
     }
 
 }
